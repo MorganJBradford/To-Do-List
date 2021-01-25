f3308c9840a4a2803bef76bfb4abf79005227e3b
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-25 13:53:50 -0600

    add addTask(), Task()
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@ function ToDoList() {
 }
 let toDoList = new ToDoList()
 
+ToDoList.prototype.addTask = function(task) {
+  task.id = this.assignId();
+  this.tasks[task.id] = task;
+}
+
+
+// Business Logic for Task
+
+function Task(task, progress) {
+  this.task = task;
+  this.progress = progress;
+}
