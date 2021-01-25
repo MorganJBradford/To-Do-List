19dd222611210080ce58ac01320bf86b7ab2c627
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-25 13:54:52 -0600

    add assignId(), findTask(), removeTask()
    Co-authored-by: Nick Lindau <nicholaithegreat@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,24 @@ ToDoList.prototype.addTask = function(task) {
   this.tasks[task.id] = task;
 }
 
+ToDoList.prototype.assignId = function() {
+  this.currentId += 1
+  return this.currentId;
+}
 
+ToDoList.prototype.findTask = function (id) {
+  if  (this.tasks[id] != undefined) {
+    return this.tasks[id];
+  }
+  return false;
+}
+ToDoList.prototype.removeTask = function(id)  {
+  if  (this.tasks[id] === undefined)  {
+    return false;
+  }
+  delete this.tasks[id];
+  return true;
+}
 // Business Logic for Task
 
 function Task(task, progress) {
